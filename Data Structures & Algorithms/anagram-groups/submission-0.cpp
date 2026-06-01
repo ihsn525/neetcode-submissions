d1bdcae06fd01d9b1b4abf765ffc519dda0733cd
ihsn525 <ihsansiju456@gmail.com>
2026-06-01 11:12:55 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> res;
+        for (int i = 0; i < strs.size(); i++) {
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            res[s].push_back(strs[i]);
+        }
+
+        vector<vector<string>> anagramsarr;
+        for (auto const& [key, val] : res) {
+            anagramsarr.push_back(val);
+        }
+        return anagramsarr;
+    }
+};
